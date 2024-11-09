274cc30b68b6990721cc1615b8fc8094bcb1f888
takahiro-maekawa <koma4024@gmail.com>
2024-11-10 00:35:44 +0900

    【フロント】セレクトボックスのカルーセル化テスト #109
    - カルーセル用のCSSを分離

--- image_viewer_app/src/app/imageViewer/app/image/layout.tsx
+++ image_viewer_app/src/app/imageViewer/app/image/layout.tsx
@@ -2,6 +2,8 @@ import Footer from "@/components/template/footer";
 import Header from "@/components/template/header";
 import { redirectIfNotHaveAccount } from '@/lib/account/rooting';
 import { redirect } from 'next/navigation';
+import './embra.css'
+
 export default async function Layout({ children }: { children: React.ReactNode }) {
   await redirectIfNotHaveAccount();
   return (
