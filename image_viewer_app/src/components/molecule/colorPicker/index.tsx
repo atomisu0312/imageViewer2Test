d689a2afcbcf600aa21a9ec84c451d39de3d5740
takahiro-maekawa <koma4024@gmail.com>
2025-05-03 17:38:08 +0900

    カラーパケットの出現位置調整 #156

--- image_viewer_app/src/components/molecule/colorPicker/index.tsx
+++ image_viewer_app/src/components/molecule/colorPicker/index.tsx
@@ -39,9 +39,9 @@ const ColorPicker: React.FC<ColorPickerProps> = ({
         />
       </div>
       {displayColorPicker && (
-        <div className="absolute z-10">
+        <div className="fixed inset-0 flex items-center justify-center z-50">
           <div 
-            className="fixed inset-0"
+            className="fixed inset-0 bg-black bg-opacity-50"
             onClick={handleClose}
           />
           <div className="relative">
